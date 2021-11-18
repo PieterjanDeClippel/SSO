c6bd33d5e3c31dbd095b87aa2a768c26953bad85
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2021-11-18 14:02:00 +0100

    Prefer using the values from the user record rather than the claims table

--- Central/Sso.Central.Data/Services/SsoProfileService.cs
+++ Central/Sso.Central.Data/Services/SsoProfileService.cs
@@ -21,10 +21,33 @@ namespace Sso.Central.Data.Services
         public async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
             var user = await userManager.GetUserAsync(context.Subject);
-            var allClaims = await userManager.GetClaimsAsync(user);
-            var filteredClaims = allClaims.Where(c => context.RequestedClaimTypes.Contains(c.Type));
-            // Optionally add more claims or compute them based on the ClaimsPrincipal properties
-            context.IssuedClaims = filteredClaims.ToList();
+            var allClaimsFromDatabase = await userManager.GetClaimsAsync(user);
+            context.IssuedClaims = context.RequestedClaimTypes
+                .Select(ct =>
+                {
+                    switch (ct)
+                    {
+                        case "name":
+                            return new System.Security.Claims.Claim("name", user.UserName);
+                        case "email":
+                            return new System.Security.Claims.Claim("email", user.Email);
+                        case "mobilephone":
+                            return new System.Security.Claims.Claim("mobilephone", user.PhoneNumber);
+                        default:
+                            return allClaimsFromDatabase.FirstOrDefault(c => c.Type == ct);
+                    }
+                })
+                .Where(c => c != null)
+                .ToList();
+
+            //var filteredClaims = allClaims.Where(c => false/*context.RequestedClaimTypes.Contains(c.Type)*/).ToList();
+
+            //// Optionally add more claims or compute them based on the ClaimsPrincipal properties
+            //filteredClaims.Add(new System.Security.Claims.Claim("name", user.UserName));
+            //filteredClaims.Add(new System.Security.Claims.Claim("email", user.Email));
+            //filteredClaims.Add(new System.Security.Claims.Claim("mobilephone", user.PhoneNumber));
+
+            //context.IssuedClaims = filteredClaims;
         }
 
         public async Task IsActiveAsync(IsActiveContext context)
