a0c04ee9ce80948133a48bb2d7d02f7cb472ae8d
DESKTOP-J4N5ARE\Pieterjan De Clippel <pieterjan@2sky.be>
2021-11-25 09:05:42 +0100

    Created new asp.net core project

--- Application/Sso.Application/Startup.cs
+++ Application/Sso.Application/Startup.cs
@@ -1,15 +1,12 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sso.Application.Data.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Sso.Application
 {
@@ -25,7 +22,7 @@ namespace Sso.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
 
             services
                 .AddAuthentication()
@@ -43,6 +40,12 @@ namespace Sso.Application
                 });
 
             services.AddSsoApplication(Configuration);
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,15 +64,33 @@ namespace Sso.Application
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
             app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
--- Application/Sso.Application/ViewModels/Account/LoginVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sso.Application.ViewModels.Account
-{
-    public class LoginVM
-    {
-        public string RedirectUrl { get; set; }
-        public string Provider { get; set; }
-    }
-}
